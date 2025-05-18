502d772b8c9a07e5a276210ed0c69c21eab4fca4
Dmitrii Seitsman <seytsman@gmail.com>
2025-05-18 22:46:49 +0300

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Dmitrii Seitsman on 18.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
