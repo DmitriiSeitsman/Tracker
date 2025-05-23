a5edc2315c2f93947560853004fe4e6ca7446c92
Dmitrii Seitsman <seytsman@gmail.com>
2025-05-23 12:08:20 +0300

    добавлено ограничение на длину ввода textField и изменен скроллинг эмоджи на горизонтальный

--- Tracker/NewHabitViewController/NewHabitViewController.swift
+++ Tracker/NewHabitViewController/NewHabitViewController.swift
@@ -138,6 +138,7 @@ final class NewHabitViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(EmojiCell.self, forCellWithReuseIdentifier: EmojiCell.reuseIdentifier)
+        
         return collectionView
     }()
     
@@ -162,6 +163,7 @@ final class NewHabitViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cancelButton.addTarget(self, action: #selector(didTapCancel), for: .touchUpInside)
         view.addSubview(scrollView)
         view.backgroundColor = .ypWhite
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -187,8 +189,8 @@ final class NewHabitViewController: UIViewController {
         scrollView.keyboardDismissMode = .onDrag
         contentView.backgroundColor = .ypWhite
         setupLayout()
-        
-        
+        nameTextField.delegate = self
+
     }
     
     // MARK: - Layout
@@ -279,6 +281,12 @@ final class NewHabitViewController: UIViewController {
     
     // MARK: - Helpers
     
+    @objc private func didTapCancel() {
+        presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+
+
+    
     private static func makeListItem(title: String, isUp: Bool) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle(title, for: .normal)
@@ -311,7 +319,7 @@ final class NewHabitViewController: UIViewController {
     
     private static func makeGridLayout() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
+        layout.scrollDirection = .horizontal
         layout.minimumInteritemSpacing = 8
         layout.minimumLineSpacing = 8
         layout.itemSize = CGSize(width: 52, height: 52)
@@ -363,7 +371,7 @@ final class EmojiCell: UICollectionViewCell {
         emojiLabel.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(emojiLabel)
         contentView.layer.cornerRadius = 16
-        contentView.backgroundColor = .ypLightGray
+        contentView.backgroundColor = .ypWhite
         NSLayoutConstraint.activate([
             emojiLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             emojiLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
@@ -396,4 +404,30 @@ final class ColorCell: UICollectionViewCell {
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+// MARK: - Extensions
+
+extension NewHabitViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text,
+              let rangeInText = Range(range, in: currentText) else {
+            return true
+        }
+
+        let updatedText = currentText.replacingCharacters(in: rangeInText, with: string)
+
+        if updatedText.count > 38 {
+            showLengthExceededAlert()
+            return false
+        }
+
+        return true
+    }
+    
+    private func showLengthExceededAlert() {
+        let alert = UIAlertController(title: nil, message: "Достигнут лимит ввода символов", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ок", style: .default))
+        present(alert, animated: true)
+    }
+
 }
