0ba2d737c9d13bfb41e2db81c8dab4ce425781d4
Dmitrii Seitsman <seytsman@gmail.com>
2025-06-30 12:12:17 +0300

    реализован онбординг

--- Tracker/SplashViewController/SplashViewController.swift
+++ Tracker/SplashViewController/SplashViewController.swift
@@ -3,14 +3,19 @@ import UIKit
 final class SplashViewController: UIViewController {
     
     private var imageView = UIImageView()
+    
     private static var window: UIWindow? {
-        return UIApplication.shared.windows.first
+        if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            return scene.windows.first
+        }
+        return nil
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("Splash загружен")
         setupUI()
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.switchToMainApp()
         }
@@ -32,18 +37,32 @@ final class SplashViewController: UIViewController {
     }
     
     private func switchToMainApp() {
-            let tabBarController = TabBarController()
-            
-            guard let window = SplashViewController.window else {
-                print("❌ Не удалось получить окно")
-                return
-            }
-
-            window.rootViewController = tabBarController
-            UIView.transition(with: window,
-                              duration: 0.4,
-                              options: .transitionCrossDissolve,
-                              animations: nil,
-                              completion: nil)
+        let hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
+        
+        guard let window = SplashViewController.window else {
+            print("❌ Не удалось получить окно")
+            return
         }
+        
+        if hasSeenOnboarding {
+            let tabBarVC = TabBarController()
+            window.rootViewController = tabBarVC
+        } else {
+            let onboardingVC = OnboardingViewController()
+            onboardingVC.onFinish = {
+                UserDefaults.standard.set(true, forKey: "hasSeenOnboarding")
+                
+                let tabBarVC = TabBarController()
+                window.rootViewController = tabBarVC
+                UIView.transition(with: window,
+                                  duration: 0.4,
+                                  options: .transitionCrossDissolve,
+                                  animations: nil,
+                                  completion: nil)
+            }
+            
+            window.rootViewController = onboardingVC
+        }
+        window.makeKeyAndVisible()
+    }
 }
